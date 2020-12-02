703796807d006116aae4e64ab99a964de2a2f402
Matias <malfaro@mbolatam.cl>
2020-12-01 22:48:30 -0300

    'backend'

--- server/routes/reserva.js
+++ server/routes/reserva.js
@@ -1,43 +1,48 @@
 let express = require('express');
 let app = express();
-
-const oracledb = require('oracledb');
 const con = require('../config/configBD');
 
 app.post('/reserva', async (req, res) => {
     console.log('POST RESERVA');
 
     try {
-        const { horaLlegada, total, idCliente, idDepartamento, fechaInicio, fechaTermino,
-            acompaniantes, adelanto } = req.body;
+        const { horaLlegada, total, idCliente, idDepartamento, fechaInicio, fechaTermino, acompaniantes, adelanto } = req.body;
 
+        
         if (horaLlegada && total && idCliente && idDepartamento && fechaInicio && fechaTermino && acompaniantes && adelanto) {
 
-            const sql = `BEGIN CREAR_RESERVA( :horaLlegada, :total, :idCliente, :idDepartamento, :fechaInicio, :fechaTermino, :acompaniantes, :adelanto, :estado, :checkIn, :checkOut ); END;`;
-            const binds = {
-                horaLlegada: horaLlegada,
-                total: total,
-                idCliente: id,
-                idDepartamento: idDepartamento,
-                fechaInicio: fechaInicio,
-                fechaTermino: fechaTermino,
-                acompaniantes: acompaniantes,
-                adelanto: adelanto,
-                estado: 1,
-                checkIn: null,
-                checkOut: null
+            const validar = await validarFecha(fechaInicio, fechaTermino, idDepartamento);
+
+            if (validar) {
+                const sql = `BEGIN CREAR_RESERVA( :horaLlegada, :total, :idCliente, :idDepartamento, :fechaInicio, :fechaTermino, :acompaniantes, :adelanto, :estado, :checkIn, :checkOut ); END;`;
+                const binds = {
+                    horaLlegada: horaLlegada,
+                    total: total,
+                    idCliente: idCliente,
+                    idDepartamento: idDepartamento,
+                    fechaInicio: fechaInicio,
+                    fechaTermino: fechaTermino,
+                    acompaniantes: acompaniantes,
+                    adelanto: adelanto,
+                    estado: 1,
+                    checkIn: null,
+                    checkOut: null
+                }
+
+                const result = await con.Open(sql, binds, true);
+
+                con.doRelease();
+
+                res.json({
+                    ok: true,
+                    mesagge: 'Operacion exitosa'
+                });
+            } else {
+                res.json({
+                    ok: false,
+                    mesagge: 'No hay cupos para esa fecha'
+                });
             }
-
-            const result = await con.Open(sql, binds, true);
-
-            con.doRelease();
-
-            res.json({
-                ok: true,
-                mesagge: 'Operacion exitosa'
-            });
-
-
         } else {
             res.json({
                 ok: false,
@@ -101,17 +106,42 @@ app.post('/serviciosextras', async (req, res) => {
     res.json(lista);
 });
 
-const validarFecha = (fecInicio, fecTermino, idDepto) => {
-    try {
-        const sql = `SELECT ID_RESERVA, FECHA_INICIO, FECHA_TERMINO FROM RESERVA WHERE ID_DEPARTAMENTO = :id`;
 
-        const binds = {id: idDepto};
+app.post('/pruebas', async (req, res) => {
+    console.log("POST PRUEBAS");
+
+    validarFecha('01/11/20', '02/11/20', 1);
+});
+
+const validarFecha = async (fecInicio, fecTermino, idDepto) => {
+    try {
+        const sql =
+        `SELECT *
+        FROM reserva
+        WHERE (fecha_inicio BETWEEN :fecInicio and :fecTermino
+        OR  fecha_termino BETWEEN :fecInicio AND :fecTermino)
+        AND id_departamento = :id`;
+
+        const binds = {
+            fecInicio: fecInicio,
+            fecTermino: fecTermino,
+            id: idDepto
+        };
 
         const result = await con.Open(sql, binds, false);
 
-        
-    } catch (ex) {
+        const resultSet = await result.rows;
 
+        if (resultSet) {
+            con.doRelease();
+            return false;
+        } else {
+            con.doRelease();
+            return true;
+        }
+
+    } catch (ex) {
+        console.log('Fallo en la funcion validarFecha: ', ex);
     }
 };
 
